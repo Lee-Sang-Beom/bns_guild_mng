e038dc38eafaf222f782be2ebe350d9f1732d8b4
LeeSangBeom <tkdqja975@naver.com>
2025-01-14 22:30:46 +0900

    firestore - storage 제거

--- datastore/firebase/firestore.ts
+++ datastore/firebase/firestore.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
