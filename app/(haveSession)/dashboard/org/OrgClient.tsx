174599045f3894f45c2a4baf19edc12147b78ea6
LeeSangBeom <tkdqja975@naver.com>
2025-02-04 15:07:55 +0900

    분배금관리 아이템 등록내용 placeholder, 안내내용 변경

--- app/(haveSession)/dashboard/org/OrgClient.tsx
+++ app/(haveSession)/dashboard/org/OrgClient.tsx
@@ -4,9 +4,14 @@ import OrgLeftTabServer from "./OrgLeftTabServer";
 import OrgRightTabServer from "./OrgRightTabServer";
 
 export default function OrgClient() {
+  const tabTitle = [
+    "권한별 문파원 리스트",
+    // "직업별 문파원 리스트",
+    "문파원 회원가입 관리",
+  ];
   return (
     <div className={ms.wrap}>
-      <Tab tabTitle={["구성 문파원", "문파원 회원가입 관리"]} color="blue">
+      <Tab tabTitle={tabTitle} color="blue">
         <OrgLeftTabServer />
         <OrgRightTabServer />
       </Tab>
