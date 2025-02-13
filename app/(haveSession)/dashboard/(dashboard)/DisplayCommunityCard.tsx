e51cd2fb2fa88a9d9e4852ae29fedb099ead0f75
LeeSangBeom <tkdqja975@naver.com>
2025-02-13 14:38:25 +0900

    대시보드 : 최근 커뮤니티정보, 최근 업데이트정보 추가

--- app/(haveSession)/dashboard/(dashboard)/DisplayCommunityCard.tsx
+++ app/(haveSession)/dashboard/(dashboard)/DisplayCommunityCard.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { NoticeResponse } from "@/types/haveSession/dashboard/notice/response";
 import ms from "./Dashboard.module.scss";
-import { useGetRecentNoticeDates } from "@/hooks/dashboard/useGetRecentNoticeDates";
 import Link from "next/link";
 import { SiDatadog } from "react-icons/si";
-export default function DisplayCashShareCard() {
-  const { data } = useGetRecentNoticeDates();
+import { useGetRecentCommunityDates } from "@/hooks/dashboard/useGetRecentCommunityDates";
+import { CommunityResponse } from "@/types/haveSession/dashboard/community/response";
+export default function DisplayCommunityCard() {
+  const { data } = useGetRecentCommunityDates();
 
   return (
     <div className={`${ms.card} ${ms.card_middle}`}>
@@ -14,25 +14,39 @@ export default function DisplayCashShareCard() {
           <li>
             <span className={`${ms.notice_title} ${ms.header}`}>제목</span>
             <span className={`${ms.notice_writerId} ${ms.header}`}>작성자</span>
+            <span className={`${ms.type} ${ms.header}`}>종류</span>
             <span className={`${ms.regDt} ${ms.header}`}>등록일</span>
           </li>
-          {data.map((noticeItem: NoticeResponse) => {
+          {data.map((communityItem: CommunityResponse) => {
             // 등록일
-            const timestamp = noticeItem.regDt;
+            const timestamp = communityItem.regDt;
             const dateString = timestamp
               ? new Date(timestamp.seconds * 1000).toLocaleString()
               : "-";
 
+            let docTypeString: string = "";
+
+            switch (communityItem.docType) {
+              case "ARTWORK":
+                docTypeString = "아트워크";
+                break;
+              case "INFO":
+                docTypeString = "정보(팁)";
+                break;
+              default:
+                break;
+            }
             return (
               <Link
-                key={`${noticeItem.docId}_link`}
-                href={`/dashboard/notice/detail?doc=${noticeItem.docId}`}
+                key={`${communityItem.docId}_link`}
+                href={`/dashboard/community/detail?doc=${communityItem.docId}`}
               >
                 <li>
-                  <span className={ms.notice_title}>{noticeItem.title}</span>
+                  <span className={ms.notice_title}>{communityItem.title}</span>
                   <span className={ms.notice_writerId}>
-                    {noticeItem.writerId}
+                    {communityItem.writerId}
                   </span>
+                  <span className={ms.type}>{docTypeString}</span>
                   <span className={ms.regDt}>{dateString}</span>
                 </li>
               </Link>
--- app/(haveSession)/dashboard/(dashboard)/DisplayUpdateCard.tsx
+++ app/(haveSession)/dashboard/(dashboard)/DisplayUpdateCard.tsx
@@ -1,12 +1,16 @@
 "use client";
-import { NoticeResponse } from "@/types/haveSession/dashboard/notice/response";
 import ms from "./Dashboard.module.scss";
-import { useGetRecentNoticeDates } from "@/hooks/dashboard/useGetRecentNoticeDates";
 import Link from "next/link";
 import { SiDatadog } from "react-icons/si";
-export default function DisplayCashShareCard() {
-  const { data } = useGetRecentNoticeDates();
+import { useEffect } from "react";
+import { useGetRecentUpdateDates } from "@/hooks/dashboard/useGetRecentUpdateDates";
+import { NoticeResponse } from "@/types/haveSession/dashboard/notice/response";
+export default function DisplayUpdateCard() {
+  const { data } = useGetRecentUpdateDates();
 
+  useEffect(() => {
+    console.log("data is ", data);
+  }, [data]);
   return (
     <div className={`${ms.card} ${ms.card_middle}`}>
       {data && data.length > 0 ? (
@@ -16,22 +20,22 @@ export default function DisplayCashShareCard() {
             <span className={`${ms.notice_writerId} ${ms.header}`}>작성자</span>
             <span className={`${ms.regDt} ${ms.header}`}>등록일</span>
           </li>
-          {data.map((noticeItem: NoticeResponse) => {
+          {data.map((updateItem: NoticeResponse) => {
             // 등록일
-            const timestamp = noticeItem.regDt;
+            const timestamp = updateItem.regDt;
             const dateString = timestamp
               ? new Date(timestamp.seconds * 1000).toLocaleString()
               : "-";
 
             return (
               <Link
-                key={`${noticeItem.docId}_link`}
-                href={`/dashboard/notice/detail?doc=${noticeItem.docId}`}
+                key={`${updateItem.docId}_link`}
+                href={`/dashboard/update/detail?doc=${updateItem.docId}`}
               >
                 <li>
-                  <span className={ms.notice_title}>{noticeItem.title}</span>
+                  <span className={ms.notice_title}>{updateItem.title}</span>
                   <span className={ms.notice_writerId}>
-                    {noticeItem.writerId}
+                    {updateItem.writerId}
                   </span>
                   <span className={ms.regDt}>{dateString}</span>
                 </li>
